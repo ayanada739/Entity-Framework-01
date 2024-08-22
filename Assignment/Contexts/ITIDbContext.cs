56b662fbdcac4daabc53305302c1bba1ef0e3850
ayanada739 <ayanada739@gmail.com>
2024-08-22 23:57:31 +0300

    Solving EF Assignment 01

---
+++ Assignment/Contexts/ITIDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Assignment.Conventions;
+using Assignment.Data_Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment.Fluent_API;
+
+namespace Assignment.Contexts
+{
+    public class ITIDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Stud_Course> Stud_Courses { get; set; }
+        public DbSet<Course_Inst> Course_Insts { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        =>
+            optionsBuilder.UseSqlServer("Server = .; Database = ITI; Trusted-Connection = True");
+        
+
+
+        
+            
+
+
+        
+    }
+}
---
+++ Assignment/Data Annotations/Instructor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Data_Annotations
+{
+    public class Instructor
+    {
+        [Key]  
+        public int ID { get; set; }
+
+        [Required]  
+        public string Name { get; set; }
+
+        public decimal Bonus { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]  
+        public decimal Salary { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal HourRate { get; set; }
+
+        public int Dept_ID { get; set; }
+    }
+
+}
